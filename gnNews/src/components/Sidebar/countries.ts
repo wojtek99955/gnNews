7310fa9cee060b11b43f22ef69ccc2de79e4b7db
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 14:56:25 +0200

    countries update

--- gnNews/src/components/Sidebar/countries.ts
+++ gnNews/src/components/Sidebar/countries.ts
@@ -1,14 +1,27 @@
 export const countries = [
   {
     country: "poland",
+    country_shortcut: "pl",
     flag_img: "https://hatscripts.github.io/circle-flags/flags/pl.svg",
   },
   {
     country: "ukraine",
+    country_shortcut: "ua",
     flag_img: "https://hatscripts.github.io/circle-flags/flags/ua.svg",
   },
   {
-    country: "gb",
-    flag_img: "https://hatscripts.github.io/circle-flags/flags/gb.svg",
+    country: "USA",
+    country_shortcut: "us",
+    flag_img: "https://hatscripts.github.io/circle-flags/flags/us.svg",
+  },
+  {
+    country: "france",
+    country_shortcut: "fr",
+    flag_img: "https://hatscripts.github.io/circle-flags/flags/fr.svg",
+  },
+  {
+    country: "spain",
+    country_shortcut: "es",
+    flag_img: "https://hatscripts.github.io/circle-flags/flags/es.svg",
   },
 ];
