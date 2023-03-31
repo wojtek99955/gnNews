4771dc6f1698635c3f1398036f756ec82382cb21
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 14:56:43 +0200

    update store

--- gnNews/src/store/store.ts
+++ gnNews/src/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { newsApiSlice } from "../api/newsApiSlice";
+import countryReducer from "../features/currentCountrySlice";
+import articlesViewReducer from "../features/articlesView/articlesViewSlice";
 
 export const store = configureStore({
   reducer: {
     [newsApiSlice.reducerPath]: newsApiSlice.reducer,
+    country: countryReducer,
+    articlesView: articlesViewReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(newsApiSlice.middleware),
