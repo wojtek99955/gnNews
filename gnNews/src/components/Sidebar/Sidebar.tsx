d6088d0d3866b08a2841ae97e1e38cde5f6e7a05
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 14:55:58 +0200

    update style

--- gnNews/src/components/Sidebar/Sidebar.tsx
+++ gnNews/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { countries } from "./countries";
+import ChooseCountry from "../../features/ChooseCountry/ChooseCountry";
 
 const Container = styled.aside`
-  min-width: 10rem;
-  /* height: calc(100vh - 4rem); */
+  height: calc(100vh - 4rem);
   position: fixed;
   top: 4rem;
+  z-index: 100;
+  background-color: #e2ecfe;
 `;
 
 interface Country {
@@ -16,37 +17,14 @@ interface Country {
 const CountriesContainer = styled.div`
   margin: auto;
   margin-top: 1rem;
-
   max-width: 6rem;
-  img {
-    width: 1.3rem;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-  li {
-    display: flex;
-    gap: 0.2rem;
-    margin-bottom: 1rem;
-    justify-content: space-between;
-  }
 `;
 
 const Sidebar = () => {
   return (
     <Container>
       <CountriesContainer>
-        <ul>
-          {countries.map(({ country, flag_img }: Country) => {
-            return (
-              <li>
-                {country} <img src={flag_img} alt="" />
-              </li>
-            );
-          })}
-        </ul>
+        <ChooseCountry />
       </CountriesContainer>
     </Container>
   );
