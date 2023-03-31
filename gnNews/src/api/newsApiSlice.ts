6e74427435b33cabb6c43a32e83f705196b2c9dc
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 13:21:54 +0200

    add query param

--- gnNews/src/api/newsApiSlice.ts
+++ gnNews/src/api/newsApiSlice.ts
@@ -9,8 +9,8 @@ export const newsApiSlice = createApi({
   tagTypes: ["News"],
   endpoints: (builder) => ({
     getNews: builder.query({
-      query: () =>
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+      query: (currenCountry) =>
+        `https://newsapi.org/v2/top-headlines?country=${currenCountry}&apiKey=${
           import.meta.env.VITE_API_KEY
         }`,
       providesTags: [{ type: "News", id: "LIST" }],
