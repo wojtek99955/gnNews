766a6ac46613c1640674404bb18a86d470230877
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 14:42:03 +0200

    add articles view

--- gnNews/src/components/Header/Header.tsx
+++ gnNews/src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import ArticlesView from "../../features/articlesView/ArticlesView";
+import Logo from "../Logo";
 
 const StyledHeader = styled.header`
   display: flex;
@@ -9,36 +11,21 @@ const StyledHeader = styled.header`
   min-height: 4rem;
   padding: 0 1rem;
   z-index: 2;
-  width: 100%; ;
-`;
-
-const Logo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.2rem;
-  span {
-    background-color: #3b7fe9;
-    padding: 0.5rem;
-    border-radius: 8px;
-    color: white;
-    font-size: 1.2rem;
-  }
-  font-weight: 700;
-  color: black;
-  font-size: 1.2rem;
+  width: 100%;
+  box-shadow: rgba(100, 100, 111, 0.25) 0px 7px 29px 0px;
 
   h1 {
     font-size: 1.5rem;
+    color: #393e46;
   }
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Logo>
-        <span>Gn</span>News
-      </Logo>
+      <Logo />
       <h1>Breaking News</h1>
+      <ArticlesView />
     </StyledHeader>
   );
 };
