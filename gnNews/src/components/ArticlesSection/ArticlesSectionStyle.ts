e56fdb1a0b7988350dac3e435cea1ea465d90a95
wojtek99955 <wojtek99955@gmail.com>
2023-03-31 14:25:01 +0200

    update style

--- gnNews/src/components/ArticlesSection/ArticlesSectionStyle.ts
+++ gnNews/src/components/ArticlesSection/ArticlesSectionStyle.ts
@@ -1,19 +1,52 @@
 import styled from "styled-components";
+import { device } from "../../assets/device";
+
+interface Props {
+  currentView: string;
+}
 
 export const Section = styled.section`
-  background-color: #ebf2fe;
-  border-top-left-radius: 20px;
+  background-color: #e2ecfe;
   width: 100%;
   min-height: calc(100vh - 4rem);
-  width: calc(100vw - 11rem);
-  left: 10rem;
-  margin-top: 4rem;
   position: relative;
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+
+  @media ${device.tablet} {
+    width: calc(100vw - 6rem);
+    left: 5rem;
+  }
 `;
 
-export const ArticlesContainer = styled.div`
+export const ArticlesContainer = styled.div<Props>`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+  grid-template-columns: ${({ currentView }) =>
+    currentView === "grid" ? "repeat(auto-fit, minmax(18rem, 1fr))" : "1fr"};
   gap: 2rem;
   padding: 2rem;
+`;
+
+export const CurrentCountry = styled.div`
+  background-color: white;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: auto;
+  border-radius: 12px;
+  margin-top: 2rem;
+
+  h2 {
+    color: #393e46;
+  }
+
+  img {
+    width: 3rem;
+  }
+`;
+
+export const Error = styled.div`
+  color: #dd5353;
 `;
